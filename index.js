ea3f8592b7ae79cf9f694189c0baec550db3e5ad
GodOfPro <1387ilia@gmail.com>
2024-02-16 01:36:55 +0330

    Add basic js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Hello Calculator From JS")
+let firstNumber = 0
+let secondNumber = 0
+let operator = "+"
+
+function operate(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case "+":
+            add(firstNumber, secondNumber);
+            break;
+        case "-":
+            subtract(firstNumber, secondNumber);
+            break;
+        case "*":
+            multiply(firstNumber, secondNumber);
+            break;
+        case "/":
+            divide(firstNumber, secondNumber);
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+

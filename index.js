cd11aff0a49044a3f6f0e7f372ba25557c4289d8
GodOfPro <1387ilia@gmail.com>
2024-02-16 23:47:05 +0330

    Add functionality to the calculator

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-let firstNumber = 0
-let secondNumber = 0
-let operator = "+"
 
 function operate(firstNumber, secondNumber, operator) {
+    let result = 0;
+
     switch (operator) {
         case "+":
-            add(firstNumber, secondNumber);
+            result = add(firstNumber, secondNumber);
             break;
         case "-":
-            subtract(firstNumber, secondNumber);
+            result = subtract(firstNumber, secondNumber);
             break;
         case "*":
-            multiply(firstNumber, secondNumber);
+            result = multiply(firstNumber, secondNumber);
             break;
         case "/":
-            divide(firstNumber, secondNumber);
+            result = divide(firstNumber, secondNumber);
     }
+
+    return result
 }
 
 function add(a, b) {
@@ -37,7 +38,34 @@ function divide(a, b) {
 const displayElement = document.querySelector(".display-text")
 const numpadContainer = document.querySelector(".numpad")
 const backspaceBtn = document.querySelector(".backspace-btn")
+const operationsContainer = document.querySelector(".operations")
+const clearBtn = document.querySelector(".clear-btn")
+const changeSignBtn = document.querySelector(".change-sign-btn")
+
 let displayValue = ""
+
+let firstNumber = 0
+let secondNumber = 0
+let operator = "+"
+
+let isFirstNumberEntered = false;
+let isSecondNumberEntered = false
+
+let isEnteringSecondNumber = false;
+
+clearBtn.addEventListener("click", () => {
+    clearCalculator();
+})
+
+changeSignBtn.addEventListener("click", () => {
+    addValueToTheStartOfTheDisplay("-")
+
+    if (!isFirstNumberEntered) {
+        firstNumber = -firstNumber
+    } else {
+        secondNumber = -secondNumber
+    }
+})
 
 numpadContainer.addEventListener("click", (e) => {
     const target = e.target
@@ -45,20 +73,79 @@ numpadContainer.addEventListener("click", (e) => {
     if (target.classList.contains("num-btn")) {
         const value = parseInt(e.target.textContent)
 
-        addValueToTheDisplay(value)
+        if (isFirstNumberEntered && !isEnteringSecondNumber) {
+            setDisplay(value)
+            isEnteringSecondNumber = true;
+        } else {
+            addValueToTheDisplay(value)
+        }
+
+        if (!isFirstNumberEntered) {
+            firstNumber = parseDisplayToNumber()
+        } else {
+            secondNumber = parseDisplayToNumber()
+        }
     }
+})
+
+operationsContainer.addEventListener("click", (e) => {
+    const targetClassList = e.target.classList
+
+
+    if (targetClassList.contains("divide-btn")) {
+        operator = "/"
+    } else if (targetClassList.contains("multiply-btn")) {
+        operator = "*"
+    } else if (targetClassList.contains("subtract-btn")) {
+        operator = "-"
+    } else if (targetClassList.contains("addition-btn")) {
+        operator = "+"
+    } else if (targetClassList.contains("equals-btn")) {
+       if (isFirstNumberEntered && isEnteringSecondNumber) {
+           console.log(firstNumber, secondNumber)
+           const result = operate(firstNumber, secondNumber, operator)
+
+           setDisplay(result)
+
+           firstNumber = result
+
+           isEnteringSecondNumber = false;
+       }
+    }
+
+    isFirstNumberEntered = true;
 })
 
 backspaceBtn.addEventListener("click", () => {
     removeValueFromTheDisplay()
 })
 
+function parseDisplayToNumber() {
+    return parseInt(displayValue)
+}
+
+function setDisplay(num) {
+    displayValue = num.toString()
+
+    updateDisplay()
+}
+
 function addValueToTheDisplay(num) {
     if (displayValue === "0") {
         displayValue = ""
     }
 
-    displayValue += num
+    displayValue += num.toString()
+
+    updateDisplay()
+}
+
+function addValueToTheStartOfTheDisplay(value) {
+    if (displayValue === "0") {
+        return;
+    }
+
+    displayValue = displayValue.padStart(displayValue.length + 1, value)
 
     updateDisplay()
 }
@@ -79,4 +166,19 @@ function removeValueFromTheDisplay() {
 
 function updateDisplay() {
     displayElement.textContent = displayValue
+}
+
+function clearCalculator() {
+    firstNumber = 0;
+    secondNumber = 0;
+
+    resetStates();
+
+    setDisplay(0)
+}
+
+function resetStates() {
+    isFirstNumberEntered = false;
+    isSecondNumberEntered = false;
+    isEnteringSecondNumber = false;
 }

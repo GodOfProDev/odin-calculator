9c4856c34538db9a3a70c8f79abe7b766da98588
GodOfPro <1387ilia@gmail.com>
2024-02-16 12:59:47 +0330

    Populate the display + Backspace button

--- index.js
+++ index.js
@@ -34,3 +34,49 @@ function divide(a, b) {
     return a / b;
 }
 
+const displayElement = document.querySelector(".display-text")
+const numpadContainer = document.querySelector(".numpad")
+const backspaceBtn = document.querySelector(".backspace-btn")
+let displayValue = ""
+
+numpadContainer.addEventListener("click", (e) => {
+    const target = e.target
+
+    if (target.classList.contains("num-btn")) {
+        const value = parseInt(e.target.textContent)
+
+        addValueToTheDisplay(value)
+    }
+})
+
+backspaceBtn.addEventListener("click", () => {
+    removeValueFromTheDisplay()
+})
+
+function addValueToTheDisplay(num) {
+    if (displayValue === "0") {
+        displayValue = ""
+    }
+
+    displayValue += num
+
+    updateDisplay()
+}
+
+function removeValueFromTheDisplay() {
+    if (displayValue.length === 0) {
+        return
+    }
+
+    displayValue = displayValue.slice(0, displayValue.length - 1)
+
+    if (displayValue.length === 0) {
+        displayValue = "0"
+    }
+
+    updateDisplay()
+}
+
+function updateDisplay() {
+    displayElement.textContent = displayValue
+}
